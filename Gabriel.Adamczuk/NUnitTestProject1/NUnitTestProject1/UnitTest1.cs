90cbb9af110b597adabf03c7c1d6064eef839e87
Gabriel172 <Gabriel172@onet.pl>
2020-10-11 11:41:14 +0200

    added test

--- Gabriel.Adamczuk/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ Gabriel.Adamczuk/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -1,14 +1,20 @@
 using NUnit.Framework;
 using System;
+using NUnit.Samples.Cash;
+
 
 namespace NUnitTestProject1
 {
     public class Tests
     {
+        private Cash f14CHF;
+
         [SetUp]
         protected void SetUp()
         {
             Console.WriteLine("This is SetUp");
+
+            f14CHF = new Cash(14, "CHF");
         }
         [TearDown]
         protected void Teardown()
@@ -42,5 +48,17 @@ namespace NUnitTestProject1
             var y = 2;
             Assert.AreNotEqual(x, y);
         }
+        /// <summary>
+        /// Assert that multiplying currency in Cash happens correctly
+        /// </summary>
+        ///
+        [Test]
+        public void SimpleMultiply()
+        {
+            // [14 CHF] *2 == [28 CHF]
+            Cash expected = new Cash(28, "CHF");
+            Assert.AreEqual(expected, f14CHF.Multiply(2));
+        }
+
     }
 }
