fe46ab91b8f647f3e32a6ee50a5415984567a16d
Igor Ojrzyński <igor@ojrzynski.eu>
2020-11-07 20:17:14 +0100

    IgorOjrzynski Laboratoria 1 Punkt 3 podpunkt g

--- IgorOjrzynski/NUnitTestProjectIgorOjrzynski/UnitTest1.cs
+++ IgorOjrzynski/NUnitTestProjectIgorOjrzynski/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace NUnitTestProjectIgorOjrzynski
@@ -7,12 +8,33 @@ namespace NUnitTestProjectIgorOjrzynski
         [SetUp]
         public void Setup()
         {
+            Console.WriteLine("This is Setup in CLI");
         }
 
+        [TearDown]
+        protected void Teardown()
+        {
+            Console.WriteLine("This is Teardown in CLI");
+        }
+
+        [Category("Smoke")]
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var x = 1;
+            var y = 2;
+
+            Assert.AreEqual(x, y);
+        }
+
+        [Category("Sanity")]
+        [Test]
+        public void Test2()
+        {
+            var x = 1;
+            var y = 2;
+
+            Assert.AreEqual(x, y);
         }
     }
 }
