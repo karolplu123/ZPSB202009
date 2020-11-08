7d0efebb5d242e0db27255b87529d4e933f91565
Damian Wąsik <dw27431@zpsb.pl>
2020-11-08 13:06:42 +0100

    Nowa zmiana

--- Damian Wąsik/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ Damian Wąsik/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -1,37 +1,29 @@
 using NUnit.Framework;
 using System;
+using NUnit.Samples.Cash;
 
 namespace NUnitTestProject1
 {
     public class Tests
     {
-       
+        private Cash f14CHF;
+
         [SetUp]
         public void Setup()
         {
-
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-
-        [SetUp]
-        protected void SetUp()
-        {
+            f14CHF = new Cash(14, "CHF");
             Console.WriteLine("This is setUp");
         }
 
+    
         [TearDown]
         protected void Teardown()
         {
             Console.WriteLine("This is Teardown");
         }
 
-        [Category("Smoke")]
         [Test]
+        [Category("Smoke")]
         public void xxx()
         {
             var x = 1;
@@ -39,8 +31,8 @@ namespace NUnitTestProject1
             Assert.AreEqual(x, y);
         }
 
-        [Category("Smoke")]
         [Test]
+        [Category("Smoke")]
         public void xxy()
         {
             var x = 1;
@@ -48,5 +40,36 @@ namespace NUnitTestProject1
             Assert.AreEqual(x, y);
         }
 
+        /// <summary>
+        /// Assert that multiplying currency in cach happens correctly
+        /// </summary>
+        [Test]
+        public void SimpleMultiply()
+        {
+            // [14 CHF] *2 == [28 CHF]
+            Cash expected = new Cash(28, "CHF");
+            Assert.AreEqual(expected, f14CHF.Multiply(2));
+        }
+
+        [Test]
+        public void SimpleAdd()
+        {
+            // [14 CHF] + [14 CHF] == [28 CHF]
+            Cash expected = new Cash(14, "CHF");
+            f14CHF.Add(expected);
+            Assert.AreEqual(expected, f14CHF);
+
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void SetCurrency_changeCurrenctToCHF_ThreeObjectCurrenciesIsAreEqual(int value)
+        {
+            Cash c2 = new Cash(value, "CHF");
+            Cash c = new Cash(value, "PLN");
+            c.SetCurrency("CHF");
+            Assert.AreEqual(c2.Currency, c.Currency);
+        }
     }
 }
