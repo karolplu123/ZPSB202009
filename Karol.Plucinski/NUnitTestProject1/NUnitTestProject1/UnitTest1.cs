3d77d057502fd99a7996adb6f469f714ea700297
Karol Pluciński <kp27089@zpsb.pl>
2020-11-08 15:33:25 +0100

    point 7

--- Karol.Plucinski/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ Karol.Plucinski/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using NUnit.Samples.Cash;
 using System;
+using Moq;
 
 namespace NUnitTestProject1
 {
@@ -54,6 +55,23 @@ namespace NUnitTestProject1
             c.SetCurrency("CHF");
 
             Assert.AreEqual(c.Currency, c2.Currency);
+        }
+
+        [Test]
+        [Category("Unit")]
+        [Category("Integration")]
+        public void TestMock()
+        {
+            //assert
+            var Cash = new Cash(2, "PLN");
+            var mockBag = new Mock<ICash>();
+            mockBag.Setup(x => x.AddMoney(It.IsAny<Cash>())).Returns(new
+           Cash(1, "CHF"));
+            //act
+            Cash.AddMoneyBag(mockBag.Object);
+            //assert
+            Assert.IsTrue(true);
+            mockBag.Verify(mock => mock.AddMoney(It.IsAny<Cash>()), Times.Once());
         }
     }
 }
