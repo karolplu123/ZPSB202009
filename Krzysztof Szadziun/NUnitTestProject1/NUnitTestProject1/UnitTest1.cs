2c3e3be4b72e04e97281a7db9ecdf16d07c3ecaf
Krzysztof Szadziun <krzysztofszadziun@gmail.com>
2020-11-08 13:37:05 +0100

    tests

--- Krzysztof Szadziun/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ Krzysztof Szadziun/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -1,8 +1,11 @@
 using NUnit.Framework;
 using System;
+using NUnit.Samples.Cash;
+
 
 namespace NUnitTestProject1
 {
+
     public class Tests
     {
         [SetUp]
@@ -34,5 +37,47 @@ namespace NUnitTestProject1
             var y = 3;
             Assert.AreEqual(x, y);
         }
+
+
+        
+        
+        /// <summary>
+        /// Test set Currency , Data-Driven Testing.
+        /// </summary>
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void TDDCheck(int value)
+        {
+            if (value == 3)
+            {
+                Assert.Fail();
+            }
+            var curr = "CHF";
+            Cash c2 = new Cash(value, "CHF");
+            Cash c = new Cash(value, "PLN");
+            c.SetCurrency("CHF");
+            Assert.AreEqual(c2.Currency, c.Currency);
+        }
+
+        public void TestCheckCurrency(int value)
+        {
+            Cash c = new Cash(value, "CHF");
+            Cash c2 = new Cash(value, "PLN");
+
+            c.SetCurrency("PLN");
+
+            Assert.AreEqual(c2.Currency, c.Currency);
+
+        }
+
+
+        
     }
+
+
+
+
+
+
 }
