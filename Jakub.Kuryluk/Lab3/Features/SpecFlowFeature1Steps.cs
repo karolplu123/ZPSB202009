9c7a743abbc990124136e47b6ce0d64d0f219b77
Jakub Kuryluk <jk27811@zpsb.pl>
2020-11-21 10:04:51 +0100

    lab 3l

--- Jakub.Kuryluk/Lab3/Features/SpecFlowFeature1Steps.cs
+++ Jakub.Kuryluk/Lab3/Features/SpecFlowFeature1Steps.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using TechTalk.SpecFlow;
 
 namespace Lab3.Features
@@ -6,10 +7,17 @@ namespace Lab3.Features
     [Binding]
     public class SpecFlowFeature1Steps
     {
+        private IWebDriver webdriver;
+        public SpecFlowFeature1Steps(IWebDriver driver)
+        {
+            webdriver = driver;
+        }
+
         [Given(@"I enter wp\.pl")]
         public void GivenIEnterWp_Pl()
         {
             //ScenarioContext.Current.Pending();
+            webdriver.Navigate().GoToUrl("http://www.wp.pl");
         }
         
         [Given(@"I click on (.*)")]
---
+++ Jakub.Kuryluk/Lab3/Hooks/Hooks1.cs
@@ -0,0 +1,43 @@
+﻿using BoDi;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Lab3.Hooks
+{
+    [Binding]
+    public sealed class Hooks1
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        private IWebDriver webdriver;
+        private readonly IObjectContainer _objectContainer;
+
+        public Hooks1(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            //TODO: implement logic that has to run before executing each scenario
+            webdriver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _objectContainer.RegisterInstanceAs<IWebDriver>(webdriver);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            //TODO: implement logic that has to run after executing each scenario
+            webdriver.Close();
+            webdriver.Dispose();
+        }
+    }
+}
