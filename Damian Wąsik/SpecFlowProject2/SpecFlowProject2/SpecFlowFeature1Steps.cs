7489a7b5b68afddbef21a37a77469bf417be0cc5
Damian Wąsik <dw27431@zpsb.pl>
2020-11-21 09:57:57 +0100

    Selenium

---
+++ Damian Wąsik/SpecFlowProject2/SpecFlowProject2/SpecFlowFeature1Steps.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject2
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private IWebDriver webdriver;
+        public SpecFlowFeature1Steps(IWebDriver driver)
+        {
+            webdriver = driver;
+        }
+
+
+        [Given(@"I enter wp\.pl")]
+        public void GivenIEnterWp_Pl()
+        {
+            webdriver.Navigate().GoToUrl("http://www.wp.pl");
+        }
+
+        [Given(@"I click on (.*)")]
+        public void GivenIClickOn(string p0)
+        {
+           // ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I fill wrong email login")]
+        public void WhenIFillWrongEmailLogin()
+        {
+          //  ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I fill wrong password")]
+        public void WhenIFillWrongPassword()
+        {
+           // ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press submit")]
+        public void WhenIPressSubmit()
+        {
+           // ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I expect to see message as „Niestety podany login lub hasło")]
+        public void ThenIExpectToSeeMessageAsNiestetyPodanyLoginLubHaslo()
+        {
+         //   ScenarioContext.Current.Pending();
+        }
+    }
+}
