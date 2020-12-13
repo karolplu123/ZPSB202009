0daa3b60b65a315bd301c33624932c842a030e67
Gabriel172 <Gabriel172@onet.pl>
2020-12-13 12:52:27 +0100

    selenium

--- Gabriel.Adamczuk/NUnitTestProject1/SpecFlowProject1/Steps/SpecFlowFeature1Steps.cs
+++ Gabriel.Adamczuk/NUnitTestProject1/SpecFlowProject1/Steps/SpecFlowFeature1Steps.cs
@@ -1,45 +1,78 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
 using TechTalk.SpecFlow;
 
-namespace SpecFlowProject1.Steps
+namespace SpecFlowSeleniumTests.Features
 {
     [Binding]
     public class SpecFlowFeature1Steps
     {
+        private IWebDriver webdriver;
+        private WebDriverWait webdriverWait;
+        public SpecFlowFeature1Steps(IWebDriver driver)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            webdriver = driver;
+            webdriverWait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+
         [Given(@"I enter wp\.pl")]
         public void GivenIEnterWp_Pl()
         {
-            //ScenarioContext.Current.Pending();
+            webdriver.Navigate().GoToUrl("http://www.wp.pl");
+            //wait for load
+            Thread.Sleep(5000);
+            var popup = "/ html / body / div[2] / div / div[2] / div[3] / div / button[2]";
+            var element = webdriverWait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(popup)));
+            // bad practice
+            Thread.Sleep(5000);
+            element.Click();
         }
-        
+
         [Given(@"I click on (.*)")]
         public void GivenIClickOn(string p0)
         {
-            //ScenarioContext.Current.Pending();
+            // bad practice
+            Thread.Sleep(5000);
+            var firstXPath = "/html/body/div[2]/div[5]/div[2]/div[3]/a[1]";
+            var elementPoczta = webdriver.FindElement(By.XPath(firstXPath));
+            elementPoczta.Click();
         }
-        
+
         [When(@"I fill wrong email login")]
         public void WhenIFillWrongEmailLogin()
         {
-            //ScenarioContext.Current.Pending();
+            var login = webdriver.FindElement(By.Id("login"));
+            login.SendKeys("Test");
         }
-        
+
         [When(@"I fill wrong password")]
         public void WhenIFillWrongPassword()
         {
-            //ScenarioContext.Current.Pending();
+            var pass = webdriver.FindElement(By.Name("password"));
+            pass.SendKeys("pomidor");
         }
-        
+
         [When(@"I press submit")]
         public void WhenIPressSubmit()
         {
-            //ScenarioContext.Current.Pending();
+            var submitBtnCssSelector = "#loginForm > div.notificationWrapper > button";
+            var submitBtn = webdriver.FindElement(By.CssSelector(submitBtnCssSelector));
+            Thread.Sleep(5000);
+            submitBtn.Click();
+            Thread.Sleep(5000);
         }
-        
+
         [Then(@"I expect to see message as „Niestety podany login lub hasło jest błędne\.”")]
         public void ThenIExpectToSeeMessageAsNiestetyPodanyLoginLubHasloJestBledne_()
         {
-            //ScenarioContext.Current.Pending();
+            string pathToMesseage = "#formError > span:nth-child(1)";
+            string didItFail = webdriver.FindElement(By.CssSelector(pathToMesseage)).Text;
+            Assert.AreEqual(didItFail, "Podany login i/lub hasło są złe.");
         }
     }
 }
---
+++ Gabriel.Adamczuk/NUnitTestProject1/SpecFlowProject1/TypeDriver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecFlowSeleniumTests
+{
+    public enum TypeDriver
+    {
+        Chrome,
+        Firefox,
+        Edge,
+        Safari,
+        InternetExplorer
+    }
+}
