7105c13ec11a89e3b3ee588864e67cebba2d0aef
DawidPartyka <dawpar5@wp.pl>
2021-02-06 18:52:09 +0100

    Added Udemy site Selenium tests

---
+++ Dawid.Partyka/unitTest/Selenium/Features/SpecFlowFeature2.feature
@@ -0,0 +1,13 @@
+﻿Feature: SpecFlowFeature2
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: As user I enter udemy.com and try login to user account
+	Given I enter udemy.com
+	And I press <login>
+	When I fill wrong email
+	And I fill incorrect password
+	And I submit the data
+	Then I expect to see message informing about unsuccessful login attempt
---
+++ Dawid.Partyka/unitTest/Selenium/Steps/UdemyTestSteps.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SpecFlowSeleniumTests.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowSeleniumTests.Features
+{
+    [Binding]
+    public class UdemyTestSteps
+    {
+        private IWebDriver webdriver;
+        private UdemyMainPage mainPage;
+        private UdemyLoginPage loginPage;
+
+        public UdemyTestSteps(IWebDriver driver)
+        {
+            webdriver = driver;
+
+            loginPage = new UdemyLoginPage(webdriver);
+            mainPage = new UdemyMainPage(webdriver);
+        }
+
+        [Given(@"I enter udemy\.com")]
+        public void GivenIEnterUdemy_Com()
+        {
+            webdriver.Navigate().GoToUrl("https://www.udemy.com/");
+        }
+        
+        [Given(@"I press (.*)")]
+        public void GivenIPress(string p0)
+        {
+            mainPage.LoginButton.Click();
+        }
+        
+        [When(@"I fill wrong email")]
+        public void WhenIFillWrongEmail()
+        {
+            loginPage.email.SendKeys("TestEmail@test.com");
+        }
+        
+        [When(@"I fill incorrect password")]
+        public void WhenIFillIncorrectPassword()
+        {
+            loginPage.pass.SendKeys("TestPassword123");
+        }
+        
+        [When(@"I submit the data")]
+        public void WhenISubmitTheData()
+        {
+            loginPage.submit.Click();
+        }
+        
+        [Then(@"I expect to see message informing about unsuccessful login attempt")]
+        public void ThenIExpectToSeeMessageInformingAboutUnsuccessfulLoginAttempt()
+        {
+            string didItFail = loginPage.failMessage.Text;
+            Assert.AreEqual(didItFail, "There was a problem logging in. Check your email and password or create an account.");
+        }
+    }
+}
