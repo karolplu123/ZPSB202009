601c8f7ebc5dd275b872a7073f510debe60bf211
wnik <wnikprojekt@gmail.com>
2021-02-11 19:45:49 +0100

    selenium

---
+++ Wojciech.Nijak/NUnitTestProject1/SpecFlowProject1/Features/CashClassUnitTestingSteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Samples.Cash;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject1.Features
+{
+    [Binding]
+    public class CashClassUnitTestingSteps
+    {
+        Cash oldCash;
+        Cash newCash;
+        [Given(@"Cash CHF (.*)")]
+        public void GivenCashCHF(int value)
+        {
+            oldCash = new Cash(value, "XXX");
+        }
+
+        [When(@"Multipy by (.*)")]
+        public void WhenMultipyBy(int multiply)
+        {
+            newCash = (Cash)oldCash.Multiply(multiply);
+        }
+
+        [Then(@"Cash Value is (.*)")]
+        public void ThenCashValueIs(int newValue)
+        {
+            Assert.AreEqual(newValue, newCash.Amount);
+        }
+
+    }
+}
